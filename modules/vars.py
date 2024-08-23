c65040c8b45daed3699f6c0fa2014122c1c2334d
altafpathan65012 <137278745+altafpathan65012@users.noreply.github.com>
2024-08-23 21:32:35 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
-api_id = 26451206
-api_hash = "32984406271d6f3945bb536671b143a7"
-bot_token = "7010963603:AAEaabkjr2DjiX2vhD8gVbkVlKWMrJTByPc"
-AUTH = 6830450483
-#Bot Username = "bhuriyabhaig"
+api_id = 23031620
+api_hash = "31cb00c1cbe580394778b43105864bca"
+bot_token = "6865731231:AAEDdXn48VOzOf0jRo0CvxkU0VxIFsikTvg"
+AUTH = 502980590
+#Bot Username = "pathansavebot"
